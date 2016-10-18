5a394c8a8d00017710dd1c94e1a6892d77504ba6
alu0100705638 <alu0100705638@ull.edu.es>
2016-10-18 19:51:35 +0100

    Probando libreria Toastr

--- dia2/Toastr/js/index.js
+++ dia2/Toastr/js/index.js
@@ -2,7 +2,7 @@
   function mensaje() {
 
     // error, success, info, warning
-    toastr.info('Aquí el mensaje de texto. || \nHola mundo esto es en otro parrafo', 'Título de notificación');
+    toastr.info('Aquí el mensaje de texto. || \\nHola mundo esto es en otro parrafo', 'Título de notificación');
 
     toastr.options = {
       closeButton: true,
