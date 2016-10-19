72fb6ccfa5fc1fe57e034ee15a0f0df717fac0c8
alu0100705638 <alu0100705638@ull.edu.es>
2016-10-19 17:17:49 +0100

    Probando cosas con la libreria Awesomeplete

---
+++ dia3/Awesomplete/js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){/* Javascript */
+  var input = document.getElementById('datos');
+  new Awesomplete(input, {
+      list: ["Ada", "Java", "JavaScript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails"]
+  });
+});
