6ad61adb28bce1f0fb577eba11c3f7b0a0689c01
alu0100705638 <alu0100705638@ull.edu.es>
2016-10-19 19:45:18 +0100

    Probando cosas con la libreria Waypoints

--- dia3/Waypoints/js/index.js
+++ dia3/Waypoints/js/index.js
@@ -3,8 +3,22 @@ $(document).ready(function(){// Nuevo waypoint
     // Elemento objetivo
     element: document.getElementById('tres'),
     // Función disparada
-    handler: function(direction) {
+    handler: mensaje();/*function(direction) {
       notify('¡Has llegado!')
-    }
+    }*/
   });
+  function mensaje() {
+
+    // error, success, info, warning
+    toastr.info('¡Has llegado');
+
+    toastr.options = {
+      closeButton: true,
+      progressBar: true,
+      positionClass: 'toast-top-right',
+      preventDuplicates: false,
+      timeOut: 2000,
+    }
+  }
+
 });
