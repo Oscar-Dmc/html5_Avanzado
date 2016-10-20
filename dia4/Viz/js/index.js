6d944cf019e2e6c751f7e6726250d7cda1d21480
ÓSCAR DAVID MARTÍN CABRERA <alu0100705638@siga.(none)>
2016-10-20 17:26:28 +0100

    Probando cosas con la libreria Viz

--- dia4/Viz/js/index.js
+++ dia4/Viz/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var sample = 'digraph g { a -> b; b -> a; b -> c; c -> c;}';
+  var sample = 'digraph g { a -> b; b -> a; b -> c; c -> c; d -> a; d -> b; d -> c; b -> d, c -> a}';
 
   var options = {
     format: 'svg',
