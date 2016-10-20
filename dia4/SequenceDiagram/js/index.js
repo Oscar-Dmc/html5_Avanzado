b6eaf3ec9e1f96fa9b23ecf8ff19151d47113256
ÓSCAR DAVID MARTÍN CABRERA <alu0100705638@siga.(none)>
2016-10-20 16:57:25 +0100

    Probando cosas con la libreria Sequence Diagrams

--- dia4/SequenceDiagram/js/index.js
+++ dia4/SequenceDiagram/js/index.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 
   var diagram = Diagram.parse(sample);
 
-  diagram.drawSVG('diagram',{
-    theme: 'hand' // o simple
+  diagram.drawSVG('diagram', {
+      theme: 'hand'    // o 'simple'
   });
 
 });
