ebf86f5e048a0f2bbd580c2983d3a9ee60dc7375
ÓSCAR DAVID MARTÍN CABRERA <alu0100705638@siga.(none)>
2016-10-20 16:59:39 +0100

    Probando cosas con la libreria Sequence Diagrams

--- dia4/SequenceDiagram/js/index.js
+++ dia4/SequenceDiagram/js/index.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
   var sample = 'A->B: Mensaje';
+  var sample1 = 'Title: Esto es un título';
+  var sample2 = 'B-->C: Línea a rayas';
+  var sample3 = 'C->>D: Línea con flecha hueca';
+  var sample4 = 'D-->>E: Línea a rayas con flecha hueca';
+  var sample5 = 'A->C: Uno \n B->C: Dos';
 
-  var diagram = Diagram.parse(sample);
+  var diagram = Diagram.parse(sample, sample1, sample2, sample3, sample4, sample5);
 
   diagram.drawSVG('diagram', {
       theme: 'hand'    // o 'simple'
