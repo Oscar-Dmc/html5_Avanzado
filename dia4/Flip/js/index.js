14eac8fc2c1651de30e8897ee71fd9e10be47179
ÓSCAR DAVID MARTÍN CABRERA <alu0100705638@siga.(none)>
2016-10-20 18:53:15 +0100

    Probando cosas con la libreria Flip

--- dia4/Flip/js/index.js
+++ dia4/Flip/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('#card').flip({
-    axis: 'xy',                // 'x' para giro en eje X
+    axis: 'y',                // 'x' para giro en eje X
     trigger: 'click',        // 'hover': giro sobre tarjeta
     speed: 500,                // Velocidad del giro
   });
