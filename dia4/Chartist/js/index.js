146176c94c894e1c735536876d490ff6b668d942
ÓSCAR DAVID MARTÍN CABRERA <alu0100705638@siga.(none)>
2016-10-20 16:31:39 +0100

    Probando cosas con la libreria Chartist

--- dia4/Chartist/js/index.js
+++ dia4/Chartist/js/index.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 
   var data = {
     labels: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
-    series: [{data: [2, 4, 6, 8, 10, 12]},
-             {data: [1, 2, 3, 4, 5, 6]},
+    series: [//{data: [2, 4, 6, 8, 10, 12]},
+             //{data: [1, 2, 3, 4, 5, 6]},
              {data: [12, 10, 8, 6, 4, 2]}
           ]
         };
   var options = {}
 
-  new Chartist.Bar('.chart', data, options);
+  new Chartist.Pie('.chart', data, options);
 });
