cf1ead07b5c78453f8aedd7ec09adec92d30ece9
ÓSCAR DAVID MARTÍN CABRERA <alu0100705638@siga.(none)>
2016-10-20 19:26:07 +0100

    Probando cosas con la libreria Panorama 3D

---
+++ dia5/Panorama/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  var PSV = PhotoSphereViewer({
+    panorama:  'https://upload.wikimedia.org/wikipedia/commons/b/b6/122_-_Toronto_-_Septembre_2009.jpg',
+    container: 'photosphere',
+    loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
+  });
+
+});
