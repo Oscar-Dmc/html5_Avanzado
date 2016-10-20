82f0d13d40921b087e49fc9f4744b33e3d198c06
ÓSCAR DAVID MARTÍN CABRERA <alu0100705638@siga.(none)>
2016-10-20 19:44:09 +0100

    Probando cosas con la libreria Panorama 3D

--- dia5/Panorama/js/index.js
+++ dia5/Panorama/js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   var PSV = PhotoSphereViewer({
-    panorama:  'https://images6.alphacoders.com/598/598358.jpg',
+    panorama:  '/html5_Avanzado/dia5/Panorama/img/598358.jpg',
     container: 'photosphere',
     loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
   });
