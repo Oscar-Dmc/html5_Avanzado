5097e61eafe7e31d0bf11e84e43b72845633bca8
alu0100705638 <alu0100705638@ull.edu.es>
2016-10-28 19:34:02 +0100

    Añadimos diferentes librerias

--- Trabajos/LibDadas/js/index.js
+++ Trabajos/LibDadas/js/index.js
@@ -1,4 +1,14 @@
-//Iniciamos la libreria impress.
-window.addEventListener('DOMContentLoaded',function(){
+$(document).ready(function(){
+  //Iniciamos la libreria impress
   impress().init();
+
+  //Libreria flip.
+  $('#card').flip({
+    axis: 'y',                // 'x' para giro en eje X
+    trigger: 'click',        // 'hover': giro sobre tarjeta
+    speed: 500,                // Velocidad del giro
+  });
+
+  //libreria before-after
+  $('.ba-slider').beforeAfter();
 });
