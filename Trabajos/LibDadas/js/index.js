eceb6db3613427d937b98a8cc41d272dd87fa50b
alu0100705638 <alu0100705638@ull.edu.es>
2016-10-29 18:51:13 +0100

    Añadiendo cambios

--- Trabajos/LibDadas/js/index.js
+++ Trabajos/LibDadas/js/index.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   //Iniciamos la libreria impress
   impress().init();
 
+  if ("ontouchstart" in document.documentElement) {
+    document.querySelector(".hint").innerHTML = "<p>Tap on the left or right to navigate</p>";
+  }
   //Libreria flip.
   $('#card').flip({
     axis: 'y',                // 'x' para giro en eje X
